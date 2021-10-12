2bc5ae1e8cd88ee3b00b3e450b40ca67197def9d
cuberiser <87647553+cuberiser@users.noreply.github.com>
2021-10-12 12:44:48 +0530

    accidentally leaked my token so edited file
    leaked token I am stupid

--- global_functions.py
+++ global_functions.py
@@ -14,7 +14,7 @@ GUILDS = [
     {"server": 824640805884919849},
 ]
 
-TOKEN = "ODY1MTIwNTQ1MTUwMjA1OTYy.YO_YSw.cA6IH-Epc3GAVI2Zb_8mUmnezKs"
+TOKEN = "accidentally leaked my token"
 
 hugs = [
     "https://i.pinimg.com/originals/f2/80/5f/f2805f274471676c96aff2bc9fbedd70.gif",
