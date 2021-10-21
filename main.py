b76a95762b11fc2acbd5c2518a86000d8cc027a1
Awsomecoder123ABC <81059687+Awsomecoder123ABC@users.noreply.github.com>
2021-10-20 21:45:28 -0500

    Added a emojify cmd

--- main.py
+++ main.py
@@ -569,10 +569,18 @@ Error Treaceback: ```py\n{exception}```""",
 
     
 @client.command()
-async def deleteemoji(ctx, emoji: nextcord.Emoji):
-	guild = ctx.guild
-	if ctx.author.guild_permissions.manage_emojis:
-		await ctx.send(f'Successfully deleted: {emoji}')
-		await emoji.delete()
+async def emojify(ctx,*,text):
+  emojis = []
+  for s in text.lower():
+    if s.isdecimal():
+      num2emo = {'0':'zero','1':'one','2':'two',
+                 '3':'three','4':'four','5':'five',
+                 '6':'six','7':'seven','8':'eight','9':'nine'}
+      emojis.append(f':{num2emo.get(s)}:')
+    elif s.isalpha():
+      emojis.append(f':regional_indicator_{s}:')
+    else:
+      emojis.append(s)
+  await ctx.send(' '.join(emojis))
 
 client.run(TOKEN)
