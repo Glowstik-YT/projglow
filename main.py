b9dff5970c5217c2dab77a1066a5a59a7bbe62d0
Awsomecoder123ABC <81059687+Awsomecoder123ABC@users.noreply.github.com>
2021-10-21 20:03:03 -0500

    Added a emojify command

--- main.py
+++ main.py
@@ -567,5 +567,21 @@ Error Treaceback: ```py\n{exception}```""",
     )  # Doing this so even when slash commands are implemented, the error handler still works just fine.
     print(exception)
 
+    
+    
+@client.command()
+async def emojify(ctx,*,text):
+  emojis = []
+  for s in text.lower():
+    if s.isdecimal():
+      num2emo = {'0':'zero','1':'one','2':'two',
+                 '3':'three','4':'four','5':'five',
+                 '6':'six','7':'seven','8':'eight','9':'nine'}
+      emojis.append(f':{num2emo.get(s)}:')
+    elif s.isalpha():
+      emojis.append(f':regional_indicator_{s}:')
+    else:
+      emojis.append(s)
+  await ctx.send(' '.join(emojis))
 
 client.run(TOKEN)
